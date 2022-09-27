d1e7bd81502cf565fe5e55cae9f29ffb7bbddd9b
boomer mitra <mitra.ashutosh49@gmail.com>
2022-09-27 10:33:11 +0530

    done

---
+++ 2D_Arrays/assignment.js
@@ -0,0 +1,10 @@
+/*Requirements: Write an efficient algorithm to search a value in a 2D matrix in which 
+the rows are in sorted order 
+and the first integer of each row is greater than the last integer of the previous row.*/
+
+let arr = [
+    [1,2,3,4],
+    [10,11,12,13],
+    [15,16,17,18],
+    [20,21,22,23]
+]
